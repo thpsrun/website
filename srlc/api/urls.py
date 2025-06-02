4e365268dd9413b32a3d18c5ff91558b6c3f93d4
ThePackle <packletwitch@gmail.com>
2025-06-02 18:35:52 -0400

    Removed trailing slashes from api/url.py

--- srlc/api/urls.py
+++ srlc/api/urls.py
@@ -13,13 +13,13 @@ from .views import (
 )
 
 urlpatterns = [
-    path("runs/<str:id>/", API_Runs.as_view(), name="Runs"),
-    path("players/<str:id>/", API_Players.as_view(), name="Players"),
+    path("runs/<str:id>", API_Runs.as_view(), name="Runs"),
+    path("players/<str:id>", API_Players.as_view(), name="Players"),
     path("players/<str:id>/pbs", API_PlayerRecords.as_view(), name="Player Records"),
-    path("games/<str:id>/", API_Games.as_view(), name="Games"),
-    path("categories/<str:id>/", API_Categories.as_view(), name="Categories"),
-    path("variables/<str:id>/", API_Variables.as_view(), name="Variables"),
-    path("values/<str:id>/", API_Values.as_view(), name="Values"),
-    path("levels/<str:id>/", API_Levels.as_view(), name="Levels"),
-    path("live/", API_Streams.as_view(), name="Streams"),
+    path("games/<str:id>", API_Games.as_view(), name="Games"),
+    path("categories/<str:id>", API_Categories.as_view(), name="Categories"),
+    path("variables/<str:id>", API_Variables.as_view(), name="Variables"),
+    path("values/<str:id>", API_Values.as_view(), name="Values"),
+    path("levels/<str:id>", API_Levels.as_view(), name="Levels"),
+    path("live", API_Streams.as_view(), name="Streams"),
 ]
